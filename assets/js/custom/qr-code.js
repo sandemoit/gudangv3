81b210df997f5636aeb7015d7257ed81ecd9c550
sandemoid <infosandemo@gmail.com>
2023-12-13 12:57:06 +0700

    fix

--- assets/js/custom/qr-code.js
+++ assets/js/custom/qr-code.js
@@ -1,15 +1,13 @@
 $(document).ready(function () {
-    function onScanSuccess(decodedText) {
-        // Jika QR code cocok, cari elemen option dengan nilai sesuai dengan hasil pemindaian
-        var option = $('#barang_id option[value="' + decodedText + '"]');
+    const barangIdSelect = document.getElementById('barang_id');
 
-        // Periksa apakah option ditemukan
-        if (option.length > 0) {
-            // Pilih option yang sesuai
-            option.prop('selected', true);
+    function onScanSuccess(decodedText) {
+        const option = Array.from(barangIdSelect.options).find(option => option.value === decodedText);
+
+        if (option) {
+            option.selected = true;
         } else {
-            // Tampilkan pesan jika ID Barang tidak ditemukan
-            console.warn('ID Barang ' + decodedText + ' Tidak Ditemukan');
+            console.warn(`ID Barang ${decodedText} tidak ditemukan dalam pilihan.`);
         }
     }
 
@@ -18,9 +16,8 @@ $(document).ready(function () {
         alert(`Gagal membuka kamera: ${error}`);
     }
 
-    var html5QrcodeScanner = new Html5QrcodeScanner(
+    const html5QrcodeScanner = new Html5QrcodeScanner(
         "reader", { fps: 10, qrbox: 300 },
-        // Tambahkan konfigurasi untuk menggunakan kamera belakang
         { facingMode: "environment" }
     );
 
